4bff15e0fd47ab851eb556d5966e4fd3f9b52f6a
Lucas <shimshaicacique@gmail.com>
2025-03-13 23:24:54 -0300

    Add files via upload

---
+++ api.php
@@ -0,0 +1,488 @@
+<?php
+$dados = [
+    'BSPAY' => [
+        [
+            "Status" => true,
+            "Client ID" => 'usuarioteste_63c4ff6423765as',
+            "Client Secret" => '1759dd06464041b182f2a18abae597a',
+            "Webhook URI" => "https://btmoney.site/gate/webhook.php"
+        ]
+    ],
+    'FIVEPAY' => [
+        [
+            "Status" => false,
+            "Client Secret" => '',
+            "Webhook URI" => "https://btmoney.site/gate/webhook.php"
+        ]
+    ],
+    'SUITPAY' => [
+        [
+            "Status" => false,
+            "Client ID" => '',
+            "Client Secret" => "",
+            "Webhook URI" => "https://btmoney.site/gate/webhook.php"
+        ]
+    ],
+    'PIXUP' => [
+        [
+            "Status" => false,
+            "Client ID" => 'JuniorMartinsp_8910138788',
+            "Client Secret" => "f3e8dd3737a4acd585545da06cf1645c5f702bc7638701c52fdd1379cc0740c2",
+            "Webhook URI" => "https://investpix.shop/gate/webhook.php"
+            ]
+        ],
+    'VENTUREPAY' => [
+            [
+                "Status" => false,
+                "Client ID" => '',
+                "Client publica" => '',
+                "Client Secret" => "",
+                "Webhook URI" => "https://btmoney.site/gate/webhook.php"
+        ]
+    ],
+];
+
+function escolherTabelaAleatoria($dados) {
+    $tabelasComStatusTrue = array_filter($dados, function($tabela) {
+        return isset($tabela[0]['Status']) && $tabela[0]['Status'] === true;
+    });
+    if (!empty($tabelasComStatusTrue)) {
+        $chaves = array_keys($tabelasComStatusTrue);
+        $chaveAleatoria = $chaves[array_rand($chaves)];
+        return [
+            'Tabela' => $chaveAleatoria,
+            'Dados' => $tabelasComStatusTrue[$chaveAleatoria][0]
+        ];
+    } else {
+        return null;
+    }
+}
+
+function logToFile($logMessage) {
+    $logFile = __DIR__ . '/daanrox_logs.txt'; 
+    $currentTime = date('Y-m-d H:i:s');
+    $logEntry = "[$currentTime] $logMessage" . PHP_EOL; 
+
+    file_put_contents($logFile, $logEntry, FILE_APPEND | LOCK_EX);
+}
+
+
+session_start();
+// header("Access-Control-Allow-Origin: http://localhost");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+$resultado = escolherTabelaAleatoria($dados);
+date_default_timezone_set('America/Sao_Paulo');
+$dueDate = date('Y-m-d H:i:s');
+$user_id = $_SESSION['user_id'];
+$numero_telefone = getUserPhoneNumber($user_id);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Recebendo JSON do corpo da requisição
+    $jsonRecebido = file_get_contents('php://input');
+    $dadosRecebidos = json_decode($jsonRecebido, true); 
+    
+        if ($resultado['Tabela'] == 'BSPAY') {
+            
+            
+            // $credentials = $resultado['Dados']['Client ID'] . ':' . $resultado['Dados']['Client Secret'];
+            $credentials = "juniormartinsp_8910138788:f3e8dd3737a4acd585545da06cf1645c5f702bc7638701c52fdd1379cc0740c2";
+            $base64_credentials = base64_encode($credentials);
+            $autenticacao = sendpost('https://api.bspay.co/v2/oauth/token', [], [
+                'Authorization: Basic '.$base64_credentials,
+                'Content-Type: application/x-www-form-urlencoded'
+            ]);
+            $data = json_decode($autenticacao, true);
+            logToFile("Data" . json_encode($data));
+            $access_token = $data['access_token'] ?? null;
+            if (!$access_token) {
+                die("Token de acesso não obtido.");
+            }
+            $response = sendpost('https://api.bspay.co/v2/pix/qrcode', json_encode(value: [
+                "amount" => $dadosRecebidos['valor'], 
+                "postbackUrl" => $resultado['Dados']['Webhook URI'], 
+                "payer" => [
+                    "name" => "Solaras Investimentos LTDA",
+                    "document" => "47288489906",
+                    "email" => "fandangosqsfd@gmail.com"
+                    ]
+                ]), [
+                    "Authorization: Bearer {$access_token}",
+                    'Content-Type: application/json',
+                    'Accept: application/json' 
+                ]);
+                logToFile("Response" . json_encode($response));
+                $res = json_decode($response, true);
+                if ( isset($res['qrcode'])) {
+                    dbOperation('insert', 'pagamentos',  [
+                        'user_id' => $user_id,
+                        'valor' => $dadosRecebidos['valor'],
+                        'cod_referencia' => $res['transactionId'],
+                        'status' => 'Pendente',
+                        'data' => $dueDate,
+                        'Banco' => $resultado['Tabela'],
+                        'numero_telefone' => $numero_telefone 
+                    ]);
+                    echo json_encode(value: [
+                        'status' => 'success',
+                        'message' => 'Solicitação de depósito enviada com sucesso.',
+                        'copiarTexto' => $res['qrcode'],
+                        'externalReference' => $res['transactionId'],
+                    ]);
+            } else {
+                echo json_encode([
+                    'status' => 'error',
+                    'message' => 'Erro ao gerar Pix.',
+                    'details' => $res,
+                ]);
+            }
+        }
+    }
+
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $jsonRecebido = file_get_contents('php://input');
+        $dadosRecebidos = json_decode($jsonRecebido, true); 
+        
+        // $payloadRecebido = json_encode($jsonRecebido, JSON_PRETTY_PRINT);
+        // logToFile("Requisição acessada pelo usuário ID: $user_id. Resultado: $payloadRecebido.");
+        // logToFile("Resultado:" . $resultado['Tabela']);
+        
+        if ($resultado['Tabela'] == 'BSPAY') {
+        $apiKey = $resultado['Dados']['Client Secret'];
+        $response = sendpost('https://api.bspay.co/v2/oauth/token', json_encode([
+            'amount' => $dadosRecebidos['valor'],
+            'client' => [
+                'name' => 'Solaras Investimentos LTDA',
+                'document' => '47288489906',
+                'telefone' => $numero_telefone,
+                'email' => 'fandangosqsfd@gmail.com',
+            ],
+            'api-key' => $apiKey,
+            'postback' => $resultado['Dados']['Webhook URI'],
+        ]), [
+            'Content-Type: application/json',
+            'Accept: application/json',
+        ]);
+    
+        $res = json_decode($response, true);
+    
+        if (isset($res['paymentCode'])) {
+            dbOperation('insert', 'pagamentos', [
+                'user_id' => $user_id,
+                'valor' => $dadosRecebidos['valor'],
+                'cod_referencia' => $res['idTransaction'],
+                'status' => 'Pendente',
+                'data' => $dueDate,
+                'Banco' => $resultado['Tabela'],
+                'numero_telefone' => $numero_telefone,
+            ]);
+    
+            echo json_encode([
+                'status' => 'success',
+                'message' => 'Solicitação de depósito enviada com sucesso.',
+                'copiarTexto' => $res['paymentCode'],
+                'externalReference' => $res['idTransaction'],
+            ]);
+        } else {
+            echo json_encode([
+                'status' => 'error',
+                'message' => 'Erro ao gerar Pix.',
+                'details' => $res,
+            ]);
+        }
+    }
+
+    if (json_last_error() === JSON_ERROR_NONE) {
+        if ($resultado['Tabela'] == 'SUITPAY') {
+            $response = sendpost('https://ws.suitpay.app/api/v1/gateway/request-qrcode', json_encode([
+                'requestNumber' => "fandangosqsfd@gmail.com",
+                'dueDate' => $dueDate,
+                'amount' => $dadosRecebidos['valor'],
+                'client' => [
+                    'name' => "Solaras Investimentos LTDA",
+                    'email' => "fandangosqsfd@gmail.com",
+                    'document' => "47288489906",
+                ],
+                'callbackUrl' => $resultado['Dados']['Webhook URI'],
+            ]), [
+                'Content-Type: application/json',
+                "ci: {$resultado['Dados']['Client ID']}",
+                "cs: {$resultado['Dados']['Client Secret']}"
+            ]);
+            $res = json_decode($response, true);
+            if ( isset($res['paymentCode'])) { 
+                dbOperation('insert', 'pagamentos',  [
+                    'user_id' => $user_id,
+                    'valor' => $dadosRecebidos['valor'],
+                    'cod_referencia' => $res['idTransaction'],
+                    'status' => 'Pendente',
+                    'data' => $dueDate,
+                    'Banco' => $resultado['Tabela'],
+                    'numero_telefone' => $numero_telefone 
+                ]);
+                echo json_encode([
+                    'status' => 'success',
+                    'message' => 'Solicitação de depósito enviada com sucesso.',
+                    'copiarTexto' => $res['paymentCode'],
+                    'externalReference' => $res['idTransaction'],
+                ]);
+            } else {
+                echo json_encode(['erro' => $res]);
+            }
+
+        } elseif ($resultado['Tabela'] == 'BSPAY') {
+                $token = 'c29sYXJhc2d0OnNob3Bzb2xhcmFzYnRj';
+                
+             
+                $valorEmCentavos = $dadosRecebidos['valor'] * 100;
+            
+            
+                $webhookResponse = sendpost('https://api.bspay.co/v2/consult-transaction', json_encode([
+                    'url' => $resultado['Dados']['Webhook URI'],
+                ]), [
+                    "Authorization: {$token}",
+                    'Content-Type: application/json',
+                    'Accept: application/json'
+                ]);
+            
+            
+                $transactionResponse = sendpost('https://venturepay.com.br/api/create/transaction/', json_encode([
+                    'amount' => $valorEmCentavos, 
+                    'nome' => 'Apl Investimentos LTDA',
+                    'cpf' => '12962752217', 
+                    'email' => 'exemplo@gmail.com',
+                    'number_phone' => '999999999', 
+                    'area_code' => '11', 
+                ]), [
+                    "Authorization: {$token}",
+                    'Content-Type: application/json',
+                    'Accept: application/json'
+                ]);
+            
+                $res = json_decode($transactionResponse, true);
+            
+            
+                if (isset($res['last_transaction']['qr_code'])) {
+                    dbOperation('insert', 'pagamentos', [
+                        'user_id' => $user_id,
+                        'valor' => $dadosRecebidos['valor'], 
+                        'cod_referencia' => $res['id'],
+                        'status' => 'Pendente',
+                        'data' => $dueDate,
+                        'Banco' => $resultado['Tabela'],
+                        'numero_telefone' => $numero_telefone 
+                    ]);
+                    echo json_encode([
+                        'status' => 'success',
+                        'webhook_response' => $webhookResponse,
+                        'message' => 'Solicitação de depósito enviada com sucesso.',
+                        'copiarTexto' => $res['last_transaction']['qr_code'],
+                        'externalReference' => $res['id'],
+                    ]);
+                    } else {
+                     
+                    echo json_encode([
+                        'status' => 'error',
+                        'message' => 'Falha ao criar a transação.',
+                        'details' => $res 
+                    ]);
+                }
+            }
+            
+                
+        } else {
+        echo json_encode(['erro' => 'JSON inválido']);
+    }
+} else {
+    echo json_encode(['erro' => 'Método não permitido, use POST']);
+}
+
+function gerarCPF() {
+    $n1 = rand(0, 9);
+    $n2 = rand(0, 9);
+    $n3 = rand(0, 9);
+    $n4 = rand(0, 9);
+    $n5 = rand(0, 9);
+    $n6 = rand(0, 9);
+    $n7 = rand(0, 9);
+    $n8 = rand(0, 9);
+    $n9 = rand(0, 9);
+
+    $d1 = 10 * $n1 + 9 * $n2 + 8 * $n3 + 7 * $n4 + 6 * $n5 + 5 * $n6 + 4 * $n7 + 3 * $n8 + 2 * $n9;
+    $d1 = 11 - ($d1 % 11);
+    $d1 = ($d1 >= 10) ? 0 : $d1;
+
+    $d2 = 11 * $n1 + 10 * $n2 + 9 * $n3 + 8 * $n4 + 7 * $n5 + 6 * $n6 + 5 * $n7 + 4 * $n8 + 3 * $n9 + 2 * $d1;
+    $d2 = 11 - ($d2 % 11);
+    $d2 = ($d2 >= 10) ? 0 : $d2;
+
+    return "$n1$n2$n3.$n4$n5$n6.$n7$n8$n9-$d1$d2";
+}
+
+
+function getUserPhoneNumber($user_id) {
+    $conn = getDBConnection();
+
+    try {
+
+        $sql = "SELECT telefone FROM usuarios WHERE id = :user_id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+        
+   
+        return $stmt->fetchColumn();
+    } catch (PDOException $e) {
+        die("Erro ao buscar número de telefone: " . $e->getMessage());
+    }
+}
+
+function sendpost($url, $data, $headers) {
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_URL => $url,
+
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_HTTPHEADER => $headers,
+    ]);
+    
+    $response = curl_exec($curl);
+    if ($response === false) {
+        throw new Exception('Erro na requisição cURL: ' . curl_error($curl));
+    }
+    curl_close($curl);
+    
+    return $response;
+}
+
+function getDBConnection() {
+    $host = 'hostinger.com.br/'; // Substitua pelo seu host
+    $db = 'solarasgt.shop'; // Substitua pelo nome do seu banco de dados
+    $user = 'fandangosqsfd@gmail.com'; // Substitua pelo seu usuário do banco de dados
+    $pass = 'Livro10%'; // Substitua pela sua senha do banco de dados
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Conexão falhou: " . $e->getMessage());
+    }
+}
+
+
+
+if (!function_exists('sendpost')) {
+    function sendpost($url, $payload, $headers) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+}
+
+
+function updateUserBalance($user_id, $amount) {
+    $conn = getDBConnection();
+    
+    try {
+        $sqlSelect = "SELECT saldo FROM usuarios WHERE id = :user_id";
+        $stmtSelect = $conn->prepare($sqlSelect);
+        $stmtSelect->bindParam(':user_id', $user_id);
+        $stmtSelect->execute();
+        $currentBalance = $stmtSelect->fetchColumn();
+
+        $newBalance = $currentBalance + $amount;
+
+        $sqlUpdate = "UPDATE usuarios SET saldo = :newBalance WHERE id = :user_id";
+        $stmtUpdate = $conn->prepare($sqlUpdate);
+        $stmtUpdate->bindParam(':newBalance', $newBalance);
+        $stmtUpdate->bindParam(':user_id', $user_id);
+
+        $stmtUpdate->execute();
+        return ['newBalance' => $newBalance];
+    } catch (PDOException $e) {
+        logError($e->getMessage());
+        return ['error' => $e->getMessage()];
+    }
+}
+
+function dbOperation($operation, $table, $data = [], $conditions = '', $columns = '*') {
+
+    $conn = getDBConnection();
+
+    try {
+        switch (strtolower($operation)) {
+            case 'insert':
+                $columns = implode(", ", array_keys($data));
+                $placeholders = ":" . implode(", :", array_keys($data));
+                $sql = "INSERT INTO $table ($columns) VALUES ($placeholders)";
+                break;
+
+            case 'select':
+                $sql = "SELECT $columns FROM $table";
+                if ($conditions) {
+                    $sql .= " WHERE $conditions";
+                }
+                break;
+
+            case 'update':
+                $set = '';
+                foreach ($data as $key => $value) {
+                    $set .= "$key = :$key, ";
+                }
+                $set = rtrim($set, ', ');
+                $sql = "UPDATE $table SET $set";
+                if ($conditions) {
+                    $sql .= " WHERE $conditions";
+                }
+                break;
+
+            case 'delete':
+                $sql = "DELETE FROM $table";
+                if ($conditions) {
+                    $sql .= " WHERE $conditions";
+                }
+                break;
+
+            default:
+                throw new Exception('Operação desconhecida');
+        }
+
+        $stmt = $conn->prepare($sql);
+
+        if ($operation == 'insert' || $operation == 'update') {
+            foreach ($data as $key => &$value) {
+                $stmt->bindParam(":$key", $value);
+            }
+        }
+
+        $stmt->execute();
+
+        if ($operation == 'select') {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        return $stmt->rowCount();
+    } catch (Exception $e) {
+        return ['error' => $e->getMessage()];
+    }
+}
+?>
---
+++ webhook.php
@@ -0,0 +1,191 @@
+<?php
+
+header(header: "Access-Control-Allow-Origin: *");
+header(header: "Content-Type: application/json");
+session_start();
+
+date_default_timezone_set(timezoneId: 'America/Sao_Paulo');
+$dueDate = date(format: 'Y-m-d H:i:s');
+
+
+require './vendor/autoload.php'; 
+
+$options = array(
+    'cluster' => 'mt1',
+    'useTLS' => true
+);
+$pusher = new Pusher\Pusher(
+    auth_key: 'usuarioteste_63c4ff6423765as',
+    secret: '1759dd06464041b182f2a18abae597a',
+    app_id: '1840990',
+    options: $options
+);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $jsonRecebido = file_get_contents(filename: 'php://input');
+    $dadosRecebidos = json_decode(json: $jsonRecebido, associative: true);
+
+    if (json_last_error() === JSON_ERROR_NONE) {
+        $banco = verificarBanco(webhookData: $dadosRecebidos);
+
+        if ($banco == 'BSPAY ') {
+            $result = processTransaction(idTransaction: $dadosRecebidos['requestBody']['transactionId'], dueDate: $dueDate, pusher: $pusher);
+        } elseif ($banco == 'SuitPay Instituição') {
+            $result = processTransaction(idTransaction: $dadosRecebidos['idTransaction'], dueDate: $dueDate, pusher: $pusher);
+        } elseif ($banco == 'FIVEPAY') {
+            $result = processTransaction(idTransaction: $dadosRecebidos['idTransaction'], dueDate: $dueDate, pusher: $pusher);
+        } elseif ($banco == 'SYNCPAY') {
+            $result = processTransaction(idTransaction: $dadosRecebidos['idTransaction'], dueDate: $dueDate, pusher: $pusher);
+        } else {
+            $result = ['error' => 'Banco desconhecido'];
+        }
+
+        echo json_encode(value: $result);
+    } else {
+        echo json_encode(value: ['error' => 'Erro no JSON recebido']);
+    }
+} else {
+    echo json_encode(value: ['error' => 'Método não permitido, use POST']);
+}
+
+function processTransaction($idTransaction, $dueDate, $pusher): void {
+    logWebhook(data: $idTransaction, logFile: 'webhook_log.txt');
+
+    $amount = getTransactionAmountFromDB($idTransaction);
+    if ($amount !== false) {
+        $result = updatePaymentStatus($idTransaction, 'Aprovado', $dueDate);
+        updateUserBalance($idTransaction, $amount);
+        $pusher->trigger('payment_channel', 'payment_approved', [
+            'transaction_id' => $idTransaction,
+            'amount' => $amount,
+        ]);
+    } else {
+        echo json_encode(value: ['error' => 'Falha ao recuperar o valor da transação.']);
+    }
+}
+
+
+function logWebhook($data, $logFile = 'webhook_log.txt'): void {
+    $jsonData = json_encode(value: $data, flags: JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    $logEntry = "[" . date(format: 'Y-m-d H:i:s') . "]\n" . $jsonData . "\n\n";
+    file_put_contents(filename: $logFile, data: $logEntry, flags: FILE_APPEND | LOCK_EX);
+}
+
+function logError($message, $logFile = 'error_log.txt'): void {
+    $logEntry = "[" . date(format: 'Y-m-d H:i:s') . "] ERROR: $message\n";
+    file_put_contents(filename: $logFile, data: $logEntry, flags: FILE_APPEND | LOCK_EX);
+}
+
+function verificarBanco($webhookData): string {
+    if (isset($webhookData['requestBody']['debitParty']['bank'])) {
+        $banco = $webhookData['requestBody']['debitParty']['bank'];
+        if (strpos(haystack: $banco, needle: 'PIXUP') !== true) {
+            return 'PixUp Soluções de Pagamentos';
+        }
+    } elseif (isset($webhookData['paymentCode']) && strpos(haystack: $webhookData['paymentCode'], needle: 'SUITPAY') !== false) {
+        return 'SuitPay Instituição';
+    }elseif (isset($webhookData['paymentMethod']) && isset($webhookData['idTransaction'])) {
+        return 'FIVEPAY';
+    } elseif (isset($webhookData['paymentMethod']) && isset($webhookData['idTransaction'])) {
+        return 'SYNCPAY';
+    }
+
+    return 'Banco desconhecido';
+}
+
+function getDBConnection(): PDO {
+    $host = 'solaras.shop'; // Substitua pelo seu host
+    $db = 'u244698307_prmd'; // Substitua pelo nome do seu banco de dados
+    $user = 'u244698307_prmd'; // Substitua pelo seu usuário do banco de dados
+    $pass = 'Livro10%'; // Substitua pela sua senha do banco de dados
+
+    try {
+        $pdo = new PDO(dsn: "mysql:host=$host;dbname=$db", username: $user, password: $pass);
+        $pdo->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        logError(message: 'Conexão falhou: ' . $e->getMessage());
+        die(json_encode(value: ['error' => 'Conexão falhou: ' . $e->getMessage()]));
+    }
+}
+
+
+function getTransactionAmountFromDB($idTransaction): bool|float {
+    $conn = getDBConnection();
+    try {
+        $sql = "SELECT valor FROM pagamentos WHERE cod_referencia = :idTransaction";
+        $stmt = $conn->prepare(query: $sql);
+        $stmt->bindParam(param: ':idTransaction', var: $idTransaction);
+        $stmt->execute();
+        $amount = $stmt->fetchColumn();
+        return $amount !== false ? (float) $amount : false;
+    } catch (PDOException $e) {
+        return false;
+    }
+}
+
+function updatePaymentStatus($idTransaction, $status, $dueDate): array {
+    $conn = getDBConnection();
+    
+    try {
+        $sql = "UPDATE pagamentos SET status = :status, data = :data WHERE cod_referencia = :idTransaction";
+        $stmt = $conn->prepare(query: $sql);
+        $stmt->bindParam(param: ':status', var: $status);
+        $stmt->bindParam(param: ':data', var: $dueDate);
+        $stmt->bindParam(param: ':idTransaction', var: $idTransaction);
+
+        $stmt->execute();
+        return ['rowsAffected' => $stmt->rowCount()];
+    } catch (PDOException $e) {
+        logError(message: $e->getMessage());
+        return ['error' => $e->getMessage()];
+    }
+}
+
+function updateUserBalance($idTransaction, $amount): array {
+    $conn = getDBConnection();
+    try {
+        
+        $sqlUser = "SELECT user_id FROM pagamentos WHERE cod_referencia = :idTransaction";
+        $stmtUser = $conn->prepare(query: $sqlUser);
+        $stmtUser->bindParam(param: ':idTransaction', var: $idTransaction);
+        $stmtUser->execute();
+        $user_id = $stmtUser->fetchColumn();
+
+        if ($user_id === false) {
+            logError(message: 'Usuário não encontrado para a transação: ' . $idTransaction);
+            return ['error' => 'Usuário não encontrado para a transação.'];
+        }
+
+        $sqlSelect = "SELECT saldo FROM usuarios WHERE id = :user_id";
+        $stmtSelect = $conn->prepare(query: $sqlSelect);
+        $stmtSelect->bindParam(param: ':user_id', var: $user_id);
+        $stmtSelect->execute();
+        $currentBalance = $stmtSelect->fetchColumn();
+
+        if ($currentBalance === false) {
+            logError(message: 'Saldo atual não encontrado para o usuário: ' . $user_id);
+            return ['error' => 'Saldo não encontrado.'];
+        }
+
+        $newBalance = $currentBalance + $amount;
+        logError(message: 'Novo saldo calculado para o usuário ' . $user_id . ': ' . $newBalance);
+
+        $sqlUpdate = "UPDATE usuarios SET saldo = :newBalance WHERE id = :user_id";
+        $stmtUpdate = $conn->prepare(query: $sqlUpdate);
+        $stmtUpdate->bindParam(param: ':newBalance', var: $newBalance);
+        $stmtUpdate->bindParam(param: ':user_id', var: $user_id);
+        $stmtUpdate->execute();
+
+        if ($stmtUpdate->rowCount() === 0) {
+            logError(message: 'Nenhuma linha foi atualizada para o usuário: ' . $user_id);
+            return ['error' => 'Falha ao atualizar o saldo.'];
+        }
+
+        return ['newBalance' => $newBalance];
+    } catch (PDOException $e) {
+        logError(message: 'Erro de banco de dados: ' . $e->getMessage());
+        return ['error' => $e->getMessage()];
+    }
+}
+
